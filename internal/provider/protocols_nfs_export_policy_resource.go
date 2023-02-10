43aa08febcf1856d6a87de9b3e4c446f34ef6168
Chris Archibald <carchi8py@gmail.com>
2023-02-10 17:43:18 -0500

    Sync bitbucket and GitHub

---
+++ internal/provider/protocols_nfs_export_policy_resource.go
@@ -0,0 +1,225 @@
+package provider
+
+import (
+	"context"
+	"fmt"
+	"strconv"
+
+	"github.com/hashicorp/terraform-plugin-framework/path"
+	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/hashicorp/terraform-plugin-log/tflog"
+	"github.com/netapp/terraform-provider-netapp-ontap/internal/interfaces"
+	"github.com/netapp/terraform-provider-netapp-ontap/internal/utils"
+)
+
+// Ensure provider defined types fully satisfy framework interfaces
+var _ resource.Resource = &ExportPolicyResource{}
+
+var _ resource.ResourceWithImportState = &ExportPolicyResource{}
+
+// NewExportPolicyResource is a helper function to simplify the provider implementation.
+func NewExportPolicyResource() resource.Resource {
+	return &ExportPolicyResource{
+		config: resourceOrDataSourceConfig{
+			name: "protocols_nfs_export_policy_resource",
+		},
+	}
+}
+
+// ExportPolicyResource defines the resource implementation.
+type ExportPolicyResource struct {
+	config resourceOrDataSourceConfig
+}
+
+// ExportPolicyResourceModel describes the resource data model.
+type ExportPolicyResourceModel struct {
+	CxProfileName types.String `tfsdk:"cx_profile_name"`
+	Name          types.String `tfsdk:"name"`
+	Vserver       types.String `tfsdk:"vserver"`
+	ID            types.String `tfsdk:"id"`
+}
+
+// Metadata returns the resource type name.
+func (r *ExportPolicyResource) Metadata(ctx context.Context, req resource.MetadataRequest, resp *resource.MetadataResponse) {
+	resp.TypeName = req.ProviderTypeName + "_" + r.config.name
+}
+
+// Schema defines the schema for the resource.
+func (r *ExportPolicyResource) Schema(ctx context.Context, req resource.SchemaRequest, resp *resource.SchemaResponse) {
+	resp.Schema = schema.Schema{
+		// This description is used by the documentation generator and the language server.
+		MarkdownDescription: "Export policy resource",
+
+		Attributes: map[string]schema.Attribute{
+			"cx_profile_name": schema.StringAttribute{
+				MarkdownDescription: "Connection profile name",
+				Required:            true,
+			},
+			"name": schema.StringAttribute{
+				MarkdownDescription: "The name of the export policy to manage",
+				Required:            true,
+			},
+			"vserver": schema.StringAttribute{
+				MarkdownDescription: "Name of the vserver to use",
+				Required:            true,
+			},
+			"id": schema.StringAttribute{
+				Computed:            true,
+				MarkdownDescription: "Export policy identifier",
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.UseStateForUnknown(),
+				},
+			},
+		},
+	}
+}
+
+// Configure adds the provider configured client to the resource.
+func (r *ExportPolicyResource) Configure(ctx context.Context, req resource.ConfigureRequest, resp *resource.ConfigureResponse) {
+	// Prevent panic if the provider has not been configured.
+	if req.ProviderData == nil {
+		return
+	}
+
+	config, ok := req.ProviderData.(Config)
+	if !ok {
+		resp.Diagnostics.AddError(
+			"Unexpected  Resource Configure Type",
+			fmt.Sprintf("Expected Config, got: %T. Please report this issue to the provider developers.", req.ProviderData),
+		)
+	}
+	r.config.providerConfig = config
+}
+
+// Create creates the resource and sets the initial Terraform state.
+func (r *ExportPolicyResource) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
+	var data *ExportPolicyResourceModel
+
+	// Read Terraform plan data into the model
+	resp.Diagnostics.Append(req.Plan.Get(ctx, &data)...)
+
+	var request interfaces.ExportpolicyResourceModel
+	errorHandler := utils.NewErrorHandler(ctx, &resp.Diagnostics)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	request.Name = data.Name.ValueString()
+
+	client, err := getRestClient(errorHandler, r.config, data.CxProfileName)
+	if err != nil {
+		// error reporting done inside NewClient
+		return
+	}
+	svm, err := interfaces.GetSvmByName(errorHandler, *client, data.Vserver.ValueString())
+	if err != nil {
+		return
+	}
+	request.Svm.Name = data.Vserver.ValueString()
+	request.Svm.UUID = svm.UUID
+
+	exportPolicy, err := interfaces.CreateExportPolicy(errorHandler, *client, request)
+	if err != nil {
+		return
+	}
+
+	data.ID = types.StringValue(strconv.Itoa(exportPolicy.ID))
+
+	tflog.Trace(ctx, "created a resource")
+
+	// Save data into Terraform state
+	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
+}
+
+// Read refreshes the Terraform state with the latest data.
+func (r *ExportPolicyResource) Read(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) {
+	var data *ExportPolicyResourceModel
+
+	// Read Terraform prior state data into the model
+	resp.Diagnostics.Append(req.State.Get(ctx, &data)...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	errorHandler := utils.NewErrorHandler(ctx, &resp.Diagnostics)
+	client, err := getRestClient(errorHandler, r.config, data.CxProfileName)
+	if err != nil {
+		// error reporting done inside NewClient
+		return
+	}
+
+	_, err = interfaces.GetExportPolicy(errorHandler, *client, data.ID.ValueString())
+	if err != nil {
+		return
+	}
+
+	// Save updated data into Terraform state
+	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
+}
+
+// Update updates the resource and sets the updated Terraform state on success.
+func (r *ExportPolicyResource) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
+	var data *ExportPolicyResourceModel
+
+	// Read Terraform plan data into the model
+	resp.Diagnostics.Append(req.Plan.Get(ctx, &data)...)
+	errorHandler := utils.NewErrorHandler(ctx, &resp.Diagnostics)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	client, err := getRestClient(errorHandler, r.config, data.CxProfileName)
+	if err != nil {
+		return
+	}
+
+	var request interfaces.ExportpolicyResourceModel
+	request.Name = data.Name.ValueString()
+
+	err = interfaces.UpdateExportPolicy(errorHandler, *client, request, data.ID.ValueString())
+	if err != nil {
+		return
+	}
+
+	// Save updated data into Terraform state
+	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+}
+
+// Delete deletes the resource and removes the Terraform state on success.
+func (r *ExportPolicyResource) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
+	var data *ExportPolicyResourceModel
+
+	// Read Terraform prior state data into the model
+	resp.Diagnostics.Append(req.State.Get(ctx, &data)...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	errorHandler := utils.NewErrorHandler(ctx, &resp.Diagnostics)
+	client, err := getRestClient(errorHandler, r.config, data.CxProfileName)
+	if err != nil {
+		// error reporting done inside NewClient
+		return
+	}
+
+	err = interfaces.DeleteExportPolicy(errorHandler, *client, data.ID.ValueString())
+	if err != nil {
+		return
+	}
+
+}
+
+// ImportState imports a resource using ID from terraform import command by calling the Read method.
+func (r *ExportPolicyResource) ImportState(ctx context.Context, req resource.ImportStateRequest, resp *resource.ImportStateResponse) {
+	resource.ImportStatePassthroughID(ctx, path.Root("id"), req, resp)
+}
