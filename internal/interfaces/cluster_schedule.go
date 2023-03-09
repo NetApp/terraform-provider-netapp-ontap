61789a94b323a1e55b08f55d8fac7bdd1c2a98a1
Chris Archibald <carchi8py@gmail.com>
2023-03-09 13:19:03 -0500

    Sync bitbucket and GitHub

--- internal/interfaces/cluster_schedule.go
+++ internal/interfaces/cluster_schedule.go
@@ -15,17 +15,24 @@ type ClusterScheduleGetDataModelONTAP struct {
 	UUID     string       `mapstructure:"uuid"`
 	Type     string       `mapstructure:"type"`
 	Scope    string       `mapstructure:"scope"`
-	Cron     CronSchedule `mapstructure:"cron",omitempty`
-	Interval string       `mapstructure:"interval",omitempty`
+	Cron     CronSchedule `mapstructure:"cron,omitempty"`
+	Interval string       `mapstructure:"interval,omitempty"`
 }
 
 // CronSchedule is the body data model for cron schedule fields
 type CronSchedule struct {
-	Hours    []int64 `mapstructure:"hours",omitempty`
-	Days     []int64 `mapstructure:"days",omitempty`
-	Minutes  []int64 `mapstructure:"minutes",omitempty`
-	Weekdays []int64 `mapstructure:"weekdays",omitempty`
-	Months   []int64 `mapstructure:"momths",omitempty`
+	Hours    []int64 `mapstructure:"hours,omitempty"`
+	Days     []int64 `mapstructure:"days,omitempty"`
+	Minutes  []int64 `mapstructure:"minutes,omitempty"`
+	Weekdays []int64 `mapstructure:"weekdays,omitempty"`
+	Months   []int64 `mapstructure:"months,omitempty"`
+}
+
+// ClusterScheduleResourceBodyDataModelONTAP describes the body data model using go types for mapping.
+type ClusterScheduleResourceBodyDataModelONTAP struct {
+	Name     string       `mapstructure:"name"`
+	Cron     CronSchedule `mapstructure:"cron,omitempty"`
+	Interval string       `mapstructure:"interval,omitempty"`
 }
 
 // GetClusterSchedule to get a single schedule info
@@ -33,7 +40,7 @@ func GetClusterSchedule(errorHandler *utils.ErrorHandler, r restclient.RestClien
 	query := r.NewQuery()
 	query.Set("name", name)
 	api := "cluster/schedules"
-	query.Fields([]string{"name", "type", "uuid", "cron", "interval", "scope"})
+	query.Fields([]string{"name", "uuid", "cron", "interval", "type", "scope"})
 
 	statusCode, response, err := r.GetNilOrOneRecord(api, query, nil)
 	if err != nil {
@@ -53,4 +60,36 @@ func GetClusterSchedule(errorHandler *utils.ErrorHandler, r restclient.RestClien
 	}
 	tflog.Debug(errorHandler.Ctx, fmt.Sprintf("Read cluster/schedules data source: %#v", dataONTAP))
 	return &dataONTAP, nil
+}
+
+// CreateClusterSchedule to create job schedule
+func CreateClusterSchedule(errorHandler *utils.ErrorHandler, r restclient.RestClient, body ClusterScheduleResourceBodyDataModelONTAP) (*ClusterScheduleGetDataModelONTAP, error) {
+	api := "cluster/schedules"
+	var bodyMap map[string]interface{}
+	if err := mapstructure.Decode(body, &bodyMap); err != nil {
+		return nil, errorHandler.MakeAndReportError("error encoding cluster_schedules body", fmt.Sprintf("error on encoding %s body: %s, body: %#v", api, err, body))
+	}
+	query := r.NewQuery()
+	query.Add("return_records", "true")
+	statusCode, response, err := r.CallCreateMethod(api, query, bodyMap)
+	if err != nil {
+		return nil, errorHandler.MakeAndReportError("error creating cluster_schedule", fmt.Sprintf("error on POST %s: %s, statusCode %d", api, err, statusCode))
+	}
+
+	var dataONTAP ClusterScheduleGetDataModelONTAP
+	if err := mapstructure.Decode(response.Records[0], &dataONTAP); err != nil {
+		return nil, errorHandler.MakeAndReportError("error decoding cluster_schedule info", fmt.Sprintf("error on decode cluster/schedule info: %s, statusCode %d, response %#v", err, statusCode, response))
+	}
+	tflog.Debug(errorHandler.Ctx, fmt.Sprintf("Create cluster_schedule source - udata: %#v", dataONTAP))
+	return &dataONTAP, nil
+}
+
+// DeleteClusterSchedule to delete job schedule
+func DeleteClusterSchedule(errorHandler *utils.ErrorHandler, r restclient.RestClient, uuid string) error {
+	api := "cluster/schedules"
+	statusCode, _, err := r.CallDeleteMethod(api+"/"+uuid, nil, nil)
+	if err != nil {
+		return errorHandler.MakeAndReportError("error deleting cluster_schedule", fmt.Sprintf("error on DELETE %s: %s, statusCode %d", api, err, statusCode))
+	}
+	return nil
 }
