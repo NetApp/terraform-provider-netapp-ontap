1affa41f210837fb99581ee48c4423a5af7fa0f7
Chris Archibald <carchi8py@gmail.com>
2023-04-11 12:34:30 -0400

    Sync bitbucket and GitHub

--- internal/interfaces/networking_ip_route.go
+++ internal/interfaces/networking_ip_route.go
@@ -22,6 +22,8 @@ type IPRouteGetDataModelONTAP struct {
 type IPRouteResourceBodyDataModelONTAP struct {
 	Destination DestinationDataSourceModel `mapstructure:"destination"`
 	SVM         Vserver                    `mapstructure:"svm"`
+	Gateway     string                     `mapstructure:"gateway"`
+	Metric      int64                      `mapstructure:"metric"`
 }
 
 // DestinationDataSourceModel describes the GET record data model using go types for mapping.
@@ -61,4 +63,36 @@ func GetIPRoute(errorHandler *utils.ErrorHandler, r restclient.RestClient, Desti
 	}
 	tflog.Debug(errorHandler.Ctx, fmt.Sprintf("Read /network/ip/routes data source: %#v", dataONTAP))
 	return &dataONTAP, nil
+}
+
+// CreateIPRoute to create net_route
+func CreateIPRoute(errorHandler *utils.ErrorHandler, r restclient.RestClient, body IPRouteResourceBodyDataModelONTAP) (*IPRouteGetDataModelONTAP, error) {
+	api := "/network/ip/routes"
+	var bodyMap map[string]interface{}
+	if err := mapstructure.Decode(body, &bodyMap); err != nil {
+		return nil, errorHandler.MakeAndReportError("error encoding /network/ip/routes body", fmt.Sprintf("error on encoding %s body: %s, body: %#v", api, err, body))
+	}
+	query := r.NewQuery()
+	query.Add("return_records", "true")
+	statusCode, response, err := r.CallCreateMethod(api, query, bodyMap)
+	if err != nil {
+		return nil, errorHandler.MakeAndReportError("error creating /network/ip/routes", fmt.Sprintf("error on POST %s: %s, statusCode %d", api, err, statusCode))
+	}
+
+	var dataONTAP IPRouteGetDataModelONTAP
+	if err := mapstructure.Decode(response.Records[0], &dataONTAP); err != nil {
+		return nil, errorHandler.MakeAndReportError("error decoding /network/ip/routes info", fmt.Sprintf("error on decode /network/ip/routes info: %s, statusCode %d, response %#v", err, statusCode, response))
+	}
+	tflog.Debug(errorHandler.Ctx, fmt.Sprintf("Create /network/ip/routes source - udata: %#v", dataONTAP))
+	return &dataONTAP, nil
+}
+
+// DeleteIPRoute to delete net_route
+func DeleteIPRoute(errorHandler *utils.ErrorHandler, r restclient.RestClient, uuid string) error {
+	api := "/network/ip/routes"
+	statusCode, _, err := r.CallDeleteMethod(api+"/"+uuid, nil, nil)
+	if err != nil {
+		return errorHandler.MakeAndReportError("error deleting /network/ip/routes", fmt.Sprintf("error on DELETE %s: %s, statusCode %d", api, err, statusCode))
+	}
+	return nil
 }
