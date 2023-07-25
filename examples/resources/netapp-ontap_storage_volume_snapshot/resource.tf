540aa832c257d8b0736c7cb2e2c764dd6cd5977d
Chris Archibald <carchi8py@gmail.com>
2023-07-25 18:30:01 +0000

    Sync bitbucket and GitHub

--- examples/resources/netapp-ontap_storage_volume_snapshot/resource.tf
+++ examples/resources/netapp-ontap_storage_volume_snapshot/resource.tf
@@ -1,6 +1,10 @@
 resource "netapp-ontap_storage_volume_snapshot_resource" "example" {
-  cx_profile_name = "cluster3"
-  name = "carchi-test-snapshot2"
-  volume_uuid = "4c3835b8-5aa9-11ed-817f-005056b3aeb3"
-
+  cx_profile_name = "cluster4"
+  name = "snaptest"
+  volume = {
+    name = "carchi_test_root"
+  }
+  svm = {
+    name = "carchi-test"
+  }
 }
