b729a8fca3a82a54b87b8293247200540084a056
Chris Archibald <carchi@netapp.com>
2023-07-26 10:26:36 -0700

    remove everything for freash resync

--- scripts/acctest.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-org_dir=$(pwd)
-
-mkdir $org_dir/test
-TF_ACC=1 go test github.com/netapp/terraform-provider-netapp-ontap/internal/provider/acceptancetests -v || { echo "Build finished in error due to failed tests" && exit 1; }
