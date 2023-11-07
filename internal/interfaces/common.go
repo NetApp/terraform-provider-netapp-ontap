43e03bb2ce1db8925173598a22242fa1e7fac67e
Chris Archibald <carchi@netapp.com>
2023-11-07 14:27:38 -0800

    fixes

--- internal/interfaces/common.go
+++ internal/interfaces/common.go
@@ -1,21 +1,7 @@
 package interfaces
 
-import (
-	"github.com/hashicorp/terraform-plugin-framework/types"
-)
-
 // NameDataModel is the standard name/uuid pair that required by most resources
 type NameDataModel struct {
 	Name string
 	UUID string
-}
-
-// StringInSlice checks if a string is in a slice of strings
-func StringInSlice(str string, list []types.String) bool {
-	for _, v := range list {
-		if v.ValueString() == str {
-			return true
-		}
-	}
-	return false
 }
