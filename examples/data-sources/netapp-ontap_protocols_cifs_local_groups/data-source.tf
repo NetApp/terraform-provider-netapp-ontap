e895f259959e5047039a14b5906b4c6da15aabef
Chuyi Ching <chuyic@netapp.com>
2023-12-05 14:53:22 -0800

    add cifs local group doc and examples

--- examples/data-sources/netapp-ontap_protocols_cifs_local_groups/data-source.tf
+++ examples/data-sources/netapp-ontap_protocols_cifs_local_groups/data-source.tf
@@ -1,6 +1,6 @@
 data "netapp-ontap_protocols_cifs_local_groups_data_source" "protocols_cifs_local_groups" {
   # required to know which system to interface with
-  cx_profile_name = "cluster1"
+  cx_profile_name = "cluster4"
   filter = {
     svm_name = "svm*"
     name     = "Administrators"
