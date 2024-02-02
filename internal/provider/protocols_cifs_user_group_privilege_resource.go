9fe328d731f918dc1b6bc2def82b5b4ac0b18507
Chuyi Ching <chuyic@netapp.com>
2024-02-01 17:42:23 -0800

    update resource validation on privileges

--- internal/provider/protocols_cifs_user_group_privilege_resource.go
+++ internal/provider/protocols_cifs_user_group_privilege_resource.go
@@ -3,25 +3,22 @@ package provider
 import (
 	"context"
 	"fmt"
+	"regexp"
 	"strings"
 
+	"github.com/hashicorp/terraform-plugin-framework-validators/setvalidator"
+	"github.com/hashicorp/terraform-plugin-framework-validators/stringvalidator"
 	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
 	"github.com/hashicorp/terraform-plugin-framework/types"
 	"github.com/hashicorp/terraform-plugin-log/tflog"
 	"github.com/netapp/terraform-provider-netapp-ontap/internal/interfaces"
 	"github.com/netapp/terraform-provider-netapp-ontap/internal/utils"
 )
-
-// TODO:
-// copy this file to match you resource (should match internal/provider/protocols_cifs_user_group_privilege_resource.go)
-// replace CifsUserGroupPrivilege with the name of the resource, following go conventions, eg IPInterface
-// replace protocols_cifs_user_group_privilege with the name of the resource, for logging purposes, eg ip_interface
-// make sure to create internal/interfaces/protocols_cifs_user_group_privilege.go too)
-// delete these 5 lines
 
 // Ensure provider defined types fully satisfy framework interfaces
 var _ resource.Resource = &CifsUserGroupPrivilegeResource{}
@@ -74,17 +71,16 @@ func (r *CifsUserGroupPrivilegeResource) Schema(ctx context.Context, req resourc
 				MarkdownDescription: "CifsUserGroupPrivilege svm name",
 				Required:            true,
 			},
-			"privileges": schema.ListAttribute{
+			"privileges": schema.SetAttribute{
 				ElementType:         types.StringType,
 				MarkdownDescription: "List of privileges",
 				Required:            true,
-				PlanModifiers: []planmodifier.List{
-					ListConvertToLowercasePlanModifier(),
+				Validators: []validator.Set{
+					setvalidator.ValueStringsAre(stringvalidator.RegexMatches(
+						regexp.MustCompile(`^[a-z]*$`),
+						"must only contain lower case characters",
+					)),
 				},
-				// Validators: []validator.List{
-				// 	// Validate this List must contain string values which are not sensitive.
-				// 	listvalidator.ValueStringsAre([]string{"SeSecurityPrivilege", "SeTakeOwnershipPrivilege", "SeChangeNotifyPrivilege"}),
-				// },
 			},
 			"id": schema.StringAttribute{
 				MarkdownDescription: "CifsUserGroupPrivilege ID",
@@ -95,80 +91,6 @@ func (r *CifsUserGroupPrivilegeResource) Schema(ctx context.Context, req resourc
 			},
 		},
 	}
-}
-
-func ListConvertToLowercasePlanModifier() planmodifier.List {
-	return &listConvertToLowercasePlanModifier{}
-}
-
-type listConvertToLowercasePlanModifier struct {
-}
-
-func (d *listConvertToLowercasePlanModifier) Description(ctx context.Context) string {
-	return "convert list of strings to lowercase"
-}
-
-func (d *listConvertToLowercasePlanModifier) MarkdownDescription(ctx context.Context) string {
-	return d.Description(ctx)
-}
-
-func (d *listConvertToLowercasePlanModifier) PlanModifyList(ctx context.Context, req planmodifier.ListRequest, resp *planmodifier.ListResponse) {
-	var attr, attrstate types.List
-
-	diags := req.Plan.GetAttribute(ctx, path.Root("privileges"), &attr)
-	resp.Diagnostics.Append(diags...)
-	if resp.Diagnostics.HasError() {
-		return
-	}
-
-	diags = req.State.GetAttribute(ctx, path.Root("privileges"), &attrstate)
-	resp.Diagnostics.Append(diags...)
-	if resp.Diagnostics.HasError() {
-		return
-	}
-
-	tflog.Debug(ctx, fmt.Sprintf("*********read a resource plan: %#v", attr))
-	tflog.Debug(ctx, fmt.Sprintf("*********read a resource state: %#v", attrstate))
-	// basetypes.ListValue{elements:[]attr.Value{basetypes.StringValue{state:0x2, value:"SeSecurityPrivilege"}, basetypes.StringValue{state:0x2, value:"SeTakeOwnershipPrivilege"}, basetypes.StringValue{state:0x2, value:"SeChangeNotifyPrivilege"}}, elementType:basetypes.StringType{}, state:0x2}
-	planValues := attr.Elements()
-	stateValues := attrstate.Elements()
-	for i, planValue := range planValues {
-		// Convert the value to lowercase for case-insensitive comparison
-		lv := strings.ToLower(planValue.(types.String).String())
-		lowerValue := types.StringValue(lv)
-		tflog.Debug(ctx, fmt.Sprintf("\t*** plan: %#v lv: %#v state: %#v", lowerValue, lv, stateValues[i]))
-		if stateValues[i].Equal(lowerValue) {
-			// If the plan value is equal to the state value, then we don't need to
-			// modify the plan.
-			tflog.Debug(ctx, fmt.Sprintf("\t*** inside plan: %#v state: %#v", lowerValue, stateValues[i]))
-			continue
-		}
-		// theValue := types.StringValue(planValue.String())
-		// lowerValue := types.String(strings.ToLower(theValue.ToTerraformValue()))
-		planValues[i] = lowerValue
-	}
-	tflog.Debug(ctx, fmt.Sprintf("*********out side plan change: %#v", planValues))
-	req.Plan.SetAttribute(ctx, path.Root("privileges"), planValues)
-
-	// resp.Diagnostics.Append(planValues)
-	// if req.PlanValue.Equal(req.StateValue) {
-	// 	// If the plan value is equal to the state value, then we don't need to
-	// 	// modify the plan.
-	// 	return
-	// }
-	//resp.SetAttribute(ctx, path.Root("privileges"), planValues)
-	// resp.Diagnostics.Append(resp.PlanValue.Elements(), planValues...)
-	// resp.Diagnostics.Append(resp.PlanValue.SetAttribute(ctx, path.Root("privileges"), planValues))
-
-	tflog.Debug(ctx, fmt.Sprintf("*********plan: %#v", planValues))
-	// Convert the value to lowercase for case-insensitive comparison
-	// for _, value := range attr.Value() {
-	// 	// Convert the value to lowercase for case-insensitive comparison
-	// 	tflog.Debug(ctx, fmt.Sprintf("read a resource: %#v", value))
-	// 	// lowerValue := strings.ToLower(value.(string))
-	// 	// updatedList.Append(lowerValue)
-	// }
-	//resp.Diagnostics.Append(resp.PlanValue.Elements()(ctx, path.Root("privileges"), req.PlanValue.Elements())...)
 }
 
 // Configure adds the provider configured client to the resource.
@@ -355,5 +277,16 @@ func (r *CifsUserGroupPrivilegeResource) Delete(ctx context.Context, req resourc
 
 // ImportState imports a resource using ID from terraform import command by calling the Read method.
 func (r *CifsUserGroupPrivilegeResource) ImportState(ctx context.Context, req resource.ImportStateRequest, resp *resource.ImportStateResponse) {
-	resource.ImportStatePassthroughID(ctx, path.Root("id"), req, resp)
+	tflog.Debug(ctx, fmt.Sprintf("import req a protocols cifs user group resource: %#v", req))
+	idParts := strings.Split(req.ID, ",")
+	if len(idParts) != 3 || idParts[0] == "" || idParts[1] == "" || idParts[2] == "" {
+		resp.Diagnostics.AddError(
+			"Unexpected Import Identifier",
+			fmt.Sprintf("Expected import identifier with format: name,svm_name,cx_profile_name. Got: %q", req.ID),
+		)
+		return
+	}
+	resp.Diagnostics.Append(resp.State.SetAttribute(ctx, path.Root("name"), idParts[0])...)
+	resp.Diagnostics.Append(resp.State.SetAttribute(ctx, path.Root("svm_name"), idParts[1])...)
+	resp.Diagnostics.Append(resp.State.SetAttribute(ctx, path.Root("cx_profile_name"), idParts[2])...)
 }
