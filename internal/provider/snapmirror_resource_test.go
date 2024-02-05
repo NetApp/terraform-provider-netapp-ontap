bda35de50d91ea4b6fb635bbee86d35bdd56124c
Suhas Bangalore Shekar <bsuhas@netapp.com>
2024-02-05 12:50:43 -0800

    adding update to ACC test

--- internal/provider/snapmirror_resource_test.go
+++ internal/provider/snapmirror_resource_test.go
@@ -16,19 +16,19 @@ func TestAccSnapmirrorResource(t *testing.T) {
 		Steps: []resource.TestStep{
 			// Test non existant Vol
 			{
-				Config:      testAccSnapmirrorResourceBasicConfig("snapmirror_dest_svm:testme", "snapmirror_source_svm:testme", "MirrorAllSnapshots"),
+				Config:      testAccSnapmirrorResourceBasicConfig("snapmirror_dest_svm:testme", "snapmirror_source_svm:testme"),
 				ExpectError: regexp.MustCompile("6619337"),
 			},
 			// Create snapmirror and read
 			{
-				Config: testAccSnapmirrorResourceBasicConfig("snapmirror_dest_svm:snap_dest", "snapmirror_source_svm:snap", "MirrorAllSnapshots"),
+				Config: testAccSnapmirrorResourceBasicConfig("snapmirror_dest_svm:snap_dest", "snapmirror_source_svm:snap"),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr("netapp-ontap_snapmirror_resource.example", "destination_endpoint.path", "snapmirror_source_svm:snap"),
 				),
 			},
 			// Update a policy
 			{
-				Config: testAccSnapmirrorResourceBasicConfig("snapmirror_dest_svm:snap_dest", "snapmirror_source_svm:snap", "MirrorAndVault"),
+				Config: testAccSnapmirrorResourceUpdateConfig("snapmirror_dest_svm:snap_dest", "snapmirror_source_svm:snap", "MirrorAndVault"),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr("netapp-ontap_snapmirror_resource.example", "policy.name", "MirrorAndVault"),
 					resource.TestCheckResourceAttr("netapp-ontap_snapmirror_resource.example", "destination_endpoint.path", "snapmirror_source_svm:snap"),
@@ -38,7 +38,39 @@ func TestAccSnapmirrorResource(t *testing.T) {
 	})
 }
 
-func testAccSnapmirrorResourceBasicConfig(sourceEndpoint string, destinationEndpoint string, policy string) string {
+func testAccSnapmirrorResourceBasicConfig(sourceEndpoint string, destinationEndpoint string) string {
+	host := os.Getenv("TF_ACC_NETAPP_HOST3")
+	admin := os.Getenv("TF_ACC_NETAPP_USER")
+	password := os.Getenv("TF_ACC_NETAPP_PASS")
+	if host == "" || admin == "" || password == "" {
+		fmt.Println("TF_ACC_NETAPP_HOST3, TF_ACC_NETAPP_USER, and TF_ACC_NETAPP_PASS must be set for acceptance tests")
+		os.Exit(1)
+	}
+	return fmt.Sprintf(`
+provider "netapp-ontap" {
+ connection_profiles = [
+    {
+      name = "cluster4"
+      hostname = "%s"
+      username = "%s"
+      password = "%s"
+      validate_certs = false
+    },
+  ]
+}
+
+resource "netapp-ontap_snapmirror_resource" "example" {
+  cx_profile_name = "cluster4"
+  source_endpoint = {
+    path = "%s"
+  }
+  destination_endpoint = {
+    path = "%s"
+  }
+}`, host, admin, password, sourceEndpoint, destinationEndpoint)
+}
+
+func testAccSnapmirrorResourceUpdateConfig(sourceEndpoint string, destinationEndpoint string, policy string) string {
 	host := os.Getenv("TF_ACC_NETAPP_HOST3")
 	admin := os.Getenv("TF_ACC_NETAPP_USER")
 	password := os.Getenv("TF_ACC_NETAPP_PASS")
