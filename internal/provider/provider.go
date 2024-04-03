11a5fd795d5d0e6927f49c1d3b5fbe7dcc66a4a8
Suhas Bangalore Shekar <bsuhas@netapp.com>
2024-04-03 11:13:42 -0700

    new data sources protocols_san_lun-map_data_source

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -26,7 +26,6 @@ type ONTAPProvider struct {
 }
 
 // ConnectionProfileModel associate a connection profile with a name
-// TODO: augment address with hostname, ...
 type ConnectionProfileModel struct {
 	Name          types.String `tfsdk:"name"`
 	Hostname      types.String `tfsdk:"hostname"`
