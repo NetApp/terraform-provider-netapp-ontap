81990929d9a16999c205e6ef7cf0d8bb0a24733b
Suhas Bangalore Shekar <bsuhas@netapp.com>
2024-04-10 10:48:14 -0700

    new resource protocols_san_igroups

--- examples/resources/netapp-ontap_protocols_san_igroup/resource.tf
+++ examples/resources/netapp-ontap_protocols_san_igroup/resource.tf
@@ -1,5 +1,10 @@
-resource "netapp-ontap_protocols_san_igroup_resource" "protocols_san_igroup" {
+resource "netapp-ontap_protocols_san_igroup_resource" "protocols_san_igroups" {
   # required to know which system to interface with
-  cx_profile_name = "cluster1"
-  name = "testme"
+  cx_profile_name = "cluster2"
+  name = "test1"
+  svm = {
+    name = "carchi-test"
+  }
+  os_type = "linux"
+  comment = "test"
 }
