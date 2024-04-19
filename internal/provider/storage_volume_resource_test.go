5d2064a267941f44e260b9f1f3d505884d04907d
swenjun <wenjun@bu.edu>
2024-04-18 17:08:06 -0700

    Edit volume acc test.

--- internal/provider/storage_volume_resource_test.go
+++ internal/provider/storage_volume_resource_test.go
@@ -108,11 +108,10 @@ resource "netapp-ontap_storage_volume_resource" "example" {
 }
 
 func testAccStorageVolumeResourceConfigUpdate(svm, volName string) string {
-	if host == "" || admin == "" || password == "" {
-		host = os.Getenv("TF_ACC_NETAPP_HOST2")
-		admin = os.Getenv("TF_ACC_NETAPP_USER")
-		password = os.Getenv("TF_ACC_NETAPP_PASS2")
-	}
+	host := os.Getenv("TF_ACC_NETAPP_HOST2")
+	admin := os.Getenv("TF_ACC_NETAPP_USER")
+	password := os.Getenv("TF_ACC_NETAPP_PASS2")
+
 	if host == "" || admin == "" || password == "" {
 		fmt.Println("TF_ACC_NETAPP_HOST, TF_ACC_NETAPP_USER, and TF_ACC_NETAPP_PASS2 must be set for acceptance tests")
 		os.Exit(1)
