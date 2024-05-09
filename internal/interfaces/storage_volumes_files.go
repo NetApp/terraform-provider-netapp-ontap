787ab0ba84dcf413357a7f6aff2d7de756e8dc17
Suhas Bangalore Shekar <bsuhas@netapp.com>
2024-05-09 10:42:11 -0700

    Data source storage_volumes_files

--- internal/interfaces/storage_volumes_files.go
+++ internal/interfaces/storage_volumes_files.go
@@ -27,12 +27,6 @@ type StorageVolumesFilesGetDataModelONTAP struct {
 	Target           string `mapstructure:"target"`
 }
 
-// StorageVolumesFilesResourceBodyDataModelONTAP describes the body data model using go types for mapping.
-type StorageVolumesFilesResourceBodyDataModelONTAP struct {
-	Name string `mapstructure:"name"`
-	SVM  svm    `mapstructure:"svm"`
-}
-
 // GetStorageVolumesFiles to get storage_volumes_files info
 func GetStorageVolumesFiles(errorHandler *utils.ErrorHandler, r restclient.RestClient, uuid string, path string) ([]StorageVolumesFilesGetDataModelONTAP, error) {
 	api := "storage/volumes/" + uuid + "/files/" + path
@@ -60,36 +54,4 @@ func GetStorageVolumesFiles(errorHandler *utils.ErrorHandler, r restclient.RestC
 	}
 	tflog.Debug(errorHandler.Ctx, fmt.Sprintf("Read storage_volumes_filess data source: %#v", dataONTAP))
 	return dataONTAP, nil
-}
-
-// CreateStorageVolumesFiles to create storage_volumes_files
-func CreateStorageVolumesFiles(errorHandler *utils.ErrorHandler, r restclient.RestClient, body StorageVolumesFilesResourceBodyDataModelONTAP) (*StorageVolumesFilesGetDataModelONTAP, error) {
-	api := "api_url"
-	var bodyMap map[string]interface{}
-	if err := mapstructure.Decode(body, &bodyMap); err != nil {
-		return nil, errorHandler.MakeAndReportError("error encoding storage_volumes_files body", fmt.Sprintf("error on encoding %s body: %s, body: %#v", api, err, body))
-	}
-	query := r.NewQuery()
-	query.Add("return_records", "true")
-	statusCode, response, err := r.CallCreateMethod(api, query, bodyMap)
-	if err != nil {
-		return nil, errorHandler.MakeAndReportError("error creating storage_volumes_files", fmt.Sprintf("error on POST %s: %s, statusCode %d", api, err, statusCode))
-	}
-
-	var dataONTAP StorageVolumesFilesGetDataModelONTAP
-	if err := mapstructure.Decode(response.Records[0], &dataONTAP); err != nil {
-		return nil, errorHandler.MakeAndReportError("error decoding storage_volumes_files info", fmt.Sprintf("error on decode storage/storage_volumes_filess info: %s, statusCode %d, response %#v", err, statusCode, response))
-	}
-	tflog.Debug(errorHandler.Ctx, fmt.Sprintf("Create storage_volumes_files source - udata: %#v", dataONTAP))
-	return &dataONTAP, nil
-}
-
-// DeleteStorageVolumesFiles to delete storage_volumes_files
-func DeleteStorageVolumesFiles(errorHandler *utils.ErrorHandler, r restclient.RestClient, uuid string) error {
-	api := "api_url"
-	statusCode, _, err := r.CallDeleteMethod(api+"/"+uuid, nil, nil)
-	if err != nil {
-		return errorHandler.MakeAndReportError("error deleting storage_volumes_files", fmt.Sprintf("error on DELETE %s: %s, statusCode %d", api, err, statusCode))
-	}
-	return nil
 }
