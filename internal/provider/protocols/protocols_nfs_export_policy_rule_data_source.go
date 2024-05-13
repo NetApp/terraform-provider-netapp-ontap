6eae3e185c075ae550cf026a4219e0749b3e5b82
Chris Archibald <carchi@netapp.com>
2024-05-13 15:22:04 -0700

    part 2

---
+++ internal/provider/protocols/protocols_nfs_export_policy_rule_data_source.go
@@ -0,0 +1,234 @@
+package protocols
+
+import (
+	"context"
+	"fmt"
+	"github.com/netapp/terraform-provider-netapp-ontap/internal/provider/connection"
+	"strconv"
+
+	"github.com/hashicorp/terraform-plugin-framework/datasource"
+	"github.com/hashicorp/terraform-plugin-framework/datasource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/netapp/terraform-provider-netapp-ontap/internal/interfaces"
+	"github.com/netapp/terraform-provider-netapp-ontap/internal/utils"
+)
+
+// Ensure provider defined types fully satisfy framework interfaces
+var _ datasource.DataSource = &ExportPolicyRuleDataSource{}
+
+// var _ resource.ResourceWithImportState = &ExportPolicyRuleResource{}
+
+// NewExportPolicyRuleDataSource is a helper function to simplify the provider implementation.
+func NewExportPolicyRuleDataSource() datasource.DataSource {
+	return &ExportPolicyRuleDataSource{
+		config: connection.ResourceOrDataSourceConfig{
+			Name: "protocols_nfs_export_policy_rule_data_source",
+		},
+	}
+}
+
+// ExportPolicyRuleDataSource defines the source implementation.
+type ExportPolicyRuleDataSource struct {
+	config connection.ResourceOrDataSourceConfig
+}
+
+// ExportPolicyRuleDataSourceModel describes the source data model.
+type ExportPolicyRuleDataSourceModel struct {
+	CxProfileName       types.String   `tfsdk:"cx_profile_name"`
+	ExportPolicyID      types.String   `tfsdk:"export_policy_id"`
+	SVMName             types.String   `tfsdk:"svm_name"`
+	ExportPolicyName    types.String   `tfsdk:"export_policy_name"`
+	RoRule              []types.String `tfsdk:"ro_rule"`
+	RwRule              []types.String `tfsdk:"rw_rule"`
+	Protocols           []types.String `tfsdk:"protocols"`
+	AnonymousUser       types.String   `tfsdk:"anonymous_user"`
+	Superuser           []types.String `tfsdk:"superuser"`
+	AllowDeviceCreation types.Bool     `tfsdk:"allow_device_creation"`
+	NtfsUnixSecurity    types.String   `tfsdk:"ntfs_unix_security"`
+	ChownMode           types.String   `tfsdk:"chown_mode"`
+	AllowSuid           types.Bool     `tfsdk:"allow_suid"`
+	ClientsMatch        []types.String `tfsdk:"clients_match"`
+	Index               types.Int64    `tfsdk:"index"`
+}
+
+// Metadata returns the resource type name.
+func (d *ExportPolicyRuleDataSource) Metadata(ctx context.Context, req datasource.MetadataRequest, resp *datasource.MetadataResponse) {
+	resp.TypeName = req.ProviderTypeName + "_" + d.config.Name
+}
+
+// Schema defines the schema for the resource.
+func (d *ExportPolicyRuleDataSource) Schema(ctx context.Context, req datasource.SchemaRequest, resp *datasource.SchemaResponse) {
+	resp.Schema = schema.Schema{
+		// This description is used by the documentation generator and the language server.
+		MarkdownDescription: "Export policy rule resource",
+
+		Attributes: map[string]schema.Attribute{
+			"cx_profile_name": schema.StringAttribute{
+				MarkdownDescription: "Connection profile name",
+				Required:            true,
+			},
+			"svm_name": schema.StringAttribute{
+				MarkdownDescription: "Name of the svm to use",
+				Required:            true,
+			},
+			"export_policy_name": schema.StringAttribute{
+				MarkdownDescription: "Export policy name",
+				Required:            true,
+			},
+			"export_policy_id": schema.StringAttribute{
+				Computed:            true,
+				MarkdownDescription: "Export policy identifier",
+			},
+			"ro_rule": schema.ListAttribute{
+				Computed:            true,
+				MarkdownDescription: "RO Access Rule",
+				ElementType:         types.StringType,
+			},
+			"rw_rule": schema.ListAttribute{
+				Computed:            true,
+				MarkdownDescription: "RW Access Rule",
+				ElementType:         types.StringType,
+			},
+			"clients_match": schema.ListAttribute{
+				Computed:            true,
+				MarkdownDescription: "List of Client Match Hostnames, IP Addresses, Netgroups, or Domains",
+				ElementType:         types.StringType,
+			},
+			"protocols": schema.ListAttribute{
+				Computed:            true,
+				MarkdownDescription: "Access Protocol",
+				ElementType:         types.StringType,
+			},
+			"anonymous_user": schema.StringAttribute{
+				MarkdownDescription: "User ID To Which Anonymous Users Are Mapped",
+				Computed:            true,
+			},
+			"superuser": schema.ListAttribute{
+				MarkdownDescription: "Superuser Security Types",
+				Computed:            true,
+				ElementType:         types.StringType,
+			},
+			"allow_device_creation": schema.BoolAttribute{
+				MarkdownDescription: "Allow Creation of Devices",
+				Computed:            true,
+			},
+			"ntfs_unix_security": schema.StringAttribute{
+				MarkdownDescription: "NTFS export UNIX security options",
+				Computed:            true,
+			},
+			"chown_mode": schema.StringAttribute{
+				MarkdownDescription: "Specifies who is authorized to change the ownership mode of a file",
+				Computed:            true,
+			},
+			"allow_suid": schema.BoolAttribute{
+				MarkdownDescription: "Honor SetUID Bits in SETATTR",
+				Computed:            true,
+			},
+			"index": schema.Int64Attribute{
+				MarkdownDescription: "rule index",
+				Required:            true,
+			},
+		},
+	}
+}
+
+// Configure adds the provider configured client to the data source.
+func (d *ExportPolicyRuleDataSource) Configure(ctx context.Context, req datasource.ConfigureRequest, resp *datasource.ConfigureResponse) {
+	// Prevent panic if the provider has not been configured.
+	if req.ProviderData == nil {
+		return
+	}
+
+	config, ok := req.ProviderData.(connection.Config)
+	if !ok {
+		resp.Diagnostics.AddError(
+			"Unexpected Data Source Configure Type",
+			fmt.Sprintf("Expected Config, got: %T. Please report this issue to the provider developers.", req.ProviderData),
+		)
+	}
+	d.config.ProviderConfig = config
+}
+
+// Read refreshes the Terraform state with the latest data.
+func (d *ExportPolicyRuleDataSource) Read(ctx context.Context, req datasource.ReadRequest, resp *datasource.ReadResponse) {
+	var data *ExportPolicyRuleDataSourceModel
+
+	// Read Terraform configuration data into the model
+	resp.Diagnostics.Append(req.Config.Get(ctx, &data)...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	errorHandler := utils.NewErrorHandler(ctx, &resp.Diagnostics)
+	client, err := connection.GetRestClient(errorHandler, d.config, data.CxProfileName)
+	if err != nil {
+		// error reporting done inside NewClient
+		return
+	}
+
+	cluster, err := interfaces.GetCluster(errorHandler, *client)
+	if err != nil {
+		// error reporting done inside GetCluster
+		return
+	}
+	if cluster == nil {
+		errorHandler.MakeAndReportError("No cluster found", fmt.Sprintf("cluster not found"))
+		return
+	}
+
+	if data.Index.IsNull() {
+		errorHandler.MakeAndReportError("error reading export policy rule", "rule index is null")
+		return
+	}
+
+	if data.ExportPolicyName.IsNull() {
+		errorHandler.MakeAndReportError("error reading export policy rule", "export policy name is null")
+		return
+	}
+
+	var exportPolicyID string
+	if data.ExportPolicyID.IsNull() {
+		filter := map[string]string{
+			"name":     data.ExportPolicyName.ValueString(),
+			"svm.name": data.SVMName.ValueString(),
+		}
+		exportPolicy, err := interfaces.GetNfsExportPolicyByName(errorHandler, *client, &filter)
+		if err != nil {
+			return
+		}
+		exportPolicyID = strconv.Itoa(exportPolicy.ID)
+	} else {
+		exportPolicyID = data.ExportPolicyID.ValueString()
+	}
+
+	restInfo, err := interfaces.GetExportPolicyRuleSingle(errorHandler, *client, exportPolicyID, data.Index.ValueInt64(), cluster.Version)
+	if err != nil {
+		return
+	}
+	if restInfo == nil {
+		errorHandler.MakeAndReportError("error reading export policy rule", "rule not found")
+		return
+	}
+
+	data.RoRule = connection.FlattenTypesStringList(restInfo.RoRule)
+	data.RwRule = connection.FlattenTypesStringList(restInfo.RwRule)
+	data.Protocols = connection.FlattenTypesStringList(restInfo.Protocols)
+	data.Superuser = connection.FlattenTypesStringList(restInfo.Superuser)
+
+	var clientsMatch []types.String
+	for _, e := range restInfo.ClientsMatch {
+		clientsMatch = append(clientsMatch, types.StringValue(e.Match))
+	}
+	data.ClientsMatch = clientsMatch
+
+	data.AllowDeviceCreation = types.BoolValue(restInfo.AllowDeviceCreation)
+	data.AllowSuid = types.BoolValue(restInfo.AllowSuid)
+	data.ChownMode = types.StringValue(restInfo.ChownMode)
+	data.NtfsUnixSecurity = types.StringValue(restInfo.NtfsUnixSecurity)
+	data.AnonymousUser = types.StringValue(restInfo.AnonymousUser)
+	data.ExportPolicyID = types.StringValue(exportPolicyID)
+
+	// Save updated data into Terraform state
+	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
+}
---
+++ internal/provider/svm/svm_peers_resource.go
@@ -0,0 +1,380 @@
+package svm
+
+import (
+	"context"
+	"fmt"
+	"github.com/netapp/terraform-provider-netapp-ontap/internal/provider/connection"
+	"github.com/netapp/terraform-provider-netapp-ontap/internal/provider/snapmirror"
+	"reflect"
+	"strings"
+
+	"github.com/hashicorp/terraform-plugin-framework/path"
+	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/hashicorp/terraform-plugin-log/tflog"
+	"github.com/netapp/terraform-provider-netapp-ontap/internal/interfaces"
+	"github.com/netapp/terraform-provider-netapp-ontap/internal/utils"
+)
+
+// Ensure provider defined types fully satisfy framework interfaces
+var _ resource.Resource = &SVMPeersResource{}
+var _ resource.ResourceWithImportState = &SVMPeersResource{}
+
+// NewSVMPeersResource is a helper function to simplify the provider implementation.
+func NewSVMPeersResource() resource.Resource {
+	return &SVMPeersResource{
+		config: connection.ResourceOrDataSourceConfig{
+			Name: "svm_peers_resource",
+		},
+	}
+}
+
+// SVMPeersResource defines the resource implementation.
+type SVMPeersResource struct {
+	config connection.ResourceOrDataSourceConfig
+}
+
+// SVMPeersResourceModel describes the resource data model.
+type SVMPeersResourceModel struct {
+	CxProfileName types.String   `tfsdk:"cx_profile_name"`
+	Applications  []types.String `tfsdk:"applications"`
+	SVM           SVM            `tfsdk:"svm"`
+	Peer          Peer           `tfsdk:"peer"`
+	ID            types.String   `tfsdk:"id"`
+	State         types.String   `tfsdk:"state"`
+}
+
+// SVM describes SVM data model.
+type SVM struct {
+	Name types.String `tfsdk:"name"`
+}
+
+// Peer describes Peer data model.
+type Peer struct {
+	SVM               SVM                `tfsdk:"svm"`
+	Cluster           snapmirror.Cluster `tfsdk:"cluster"`
+	PeerCxProfileName types.String       `tfsdk:"peer_cx_profile_name"`
+}
+
+// Metadata returns the resource type name.
+func (r *SVMPeersResource) Metadata(ctx context.Context, req resource.MetadataRequest, resp *resource.MetadataResponse) {
+	resp.TypeName = req.ProviderTypeName + "_" + r.config.Name
+}
+
+// Schema defines the schema for the resource.
+func (r *SVMPeersResource) Schema(ctx context.Context, req resource.SchemaRequest, resp *resource.SchemaResponse) {
+	resp.Schema = schema.Schema{
+		// This description is used by the documentation generator and the language server.
+		MarkdownDescription: "SVMPeers resource",
+
+		Attributes: map[string]schema.Attribute{
+			"cx_profile_name": schema.StringAttribute{
+				MarkdownDescription: "Connection profile name",
+				Required:            true,
+			},
+			"applications": schema.SetAttribute{
+				MarkdownDescription: "SVMPeering applications",
+				Required:            true,
+				ElementType:         types.StringType,
+			},
+			"svm": schema.SingleNestedAttribute{
+				MarkdownDescription: "SVM details for SVMPeer",
+				Required:            true,
+				Attributes: map[string]schema.Attribute{
+					"name": schema.StringAttribute{
+						MarkdownDescription: "name of the SVM",
+						Required:            true,
+					},
+				},
+			},
+			"peer": schema.SingleNestedAttribute{
+				MarkdownDescription: "Peer details for SVMPeer",
+				Required:            true,
+				Attributes: map[string]schema.Attribute{
+					"svm": schema.SingleNestedAttribute{
+						MarkdownDescription: "peer SVM details for SVMPeer",
+						Required:            true,
+						Attributes: map[string]schema.Attribute{
+							"name": schema.StringAttribute{
+								MarkdownDescription: "name of the peer SVM",
+								Required:            true,
+							},
+						},
+					},
+					"cluster": schema.SingleNestedAttribute{
+						MarkdownDescription: "peer Cluster details for SVMPeer",
+						Required:            true,
+						Attributes: map[string]schema.Attribute{
+							"name": schema.StringAttribute{
+								MarkdownDescription: "name of the peer Cluster",
+								Required:            true,
+							},
+						},
+					},
+					"peer_cx_profile_name": schema.StringAttribute{
+						MarkdownDescription: "Peer connection profile name, if not provided, status will be only initiated and need to be accepted from peer side to make the status peered",
+						Optional:            true,
+					},
+				},
+			},
+			"state": schema.StringAttribute{
+				Computed: true,
+			},
+			"id": schema.StringAttribute{
+				MarkdownDescription: "SVMPeers UUID",
+				Computed:            true,
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.UseStateForUnknown(),
+				},
+			},
+		},
+	}
+}
+
+// Configure adds the provider configured client to the resource.
+func (r *SVMPeersResource) Configure(ctx context.Context, req resource.ConfigureRequest, resp *resource.ConfigureResponse) {
+	// Prevent panic if the provider has not been configured.
+	if req.ProviderData == nil {
+		return
+	}
+	config, ok := req.ProviderData.(connection.Config)
+	if !ok {
+		resp.Diagnostics.AddError(
+			"Unexpected Resource Configure Type",
+			fmt.Sprintf("Expected Config, got: %T. Please report this issue to the provider developers.", req.ProviderData),
+		)
+	}
+	r.config.ProviderConfig = config
+}
+
+// Read refreshes the Terraform state with the latest data.
+func (r *SVMPeersResource) Read(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) {
+	var data SVMPeersResourceModel
+
+	// Read Terraform prior state data into the model
+	resp.Diagnostics.Append(req.State.Get(ctx, &data)...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	errorHandler := utils.NewErrorHandler(ctx, &resp.Diagnostics)
+	// we need to defer setting the client until we can read the connection profile name
+	client, err := connection.GetRestClient(errorHandler, r.config, data.CxProfileName)
+	if err != nil {
+		// error reporting done inside NewClient
+		return
+	}
+
+	tflog.Debug(ctx, fmt.Sprintf("read a SVMPeer resource: %#v", data))
+	var restInfo *interfaces.SVMPeerDataSourceModel
+	if data.ID.ValueString() != "" {
+		restInfo, err = interfaces.GetSVMPeer(errorHandler, *client, data.ID.ValueString())
+		if err != nil {
+			// error reporting done inside GetSVMPeer
+			return
+		}
+	} else {
+		restInfo, err = interfaces.GetSVMPeersBySVMNameAndPeerSvmName(errorHandler, *client, data.SVM.Name.ValueString(), data.Peer.SVM.Name.ValueString())
+		if err != nil {
+			// error reporting done inside GetSVMPeersBySVMNameAndPeerSvmName
+			return
+		}
+		data.Peer.Cluster.Name = types.StringValue(restInfo.Peer.Cluster.Name)
+	}
+
+	if restInfo == nil {
+		errorHandler.MakeAndReportError("error reading info", "No SVM Peer found")
+	}
+
+	data.ID = types.StringValue(restInfo.UUID)
+	data.State = types.StringValue(restInfo.State)
+	var applications []types.String
+	for _, e := range restInfo.Applications {
+		applications = append(applications, types.StringValue(e))
+	}
+	data.Applications = applications
+
+	// Save data into Terraform state
+	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
+}
+
+// Create a resource and retrieve UUID
+func (r *SVMPeersResource) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
+	var data *SVMPeersResourceModel
+
+	// Read Terraform plan data into the model
+	resp.Diagnostics.Append(req.Plan.Get(ctx, &data)...)
+
+	errorHandler := utils.NewErrorHandler(ctx, &resp.Diagnostics)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	var request interfaces.SVMPeerResourceModel
+	var applications []string
+	for _, e := range data.Applications {
+		applications = append(applications, e.ValueString())
+	}
+	request.Applications = applications
+	request.SVM.Name = data.SVM.Name.ValueString()
+	request.Peer.SVM.Name = data.Peer.SVM.Name.ValueString()
+	request.Peer.Cluster.Name = data.Peer.Cluster.Name.ValueString()
+
+	client, err := connection.GetRestClient(errorHandler, r.config, data.CxProfileName)
+	if err != nil {
+		// error reporting done inside NewClient
+		return
+	}
+
+	resource, err := interfaces.CreateSVMPeers(errorHandler, *client, request)
+	if err != nil {
+		return
+	}
+
+	data.ID = types.StringValue(resource.UUID)
+	var restInfo *interfaces.SVMPeerDataSourceModel
+	if !data.Peer.PeerCxProfileName.IsNull() {
+		peerClient, err := connection.GetRestClient(errorHandler, r.config, data.Peer.PeerCxProfileName)
+		if err != nil {
+			// error reporting done inside NewClient
+			return
+		}
+		var body interfaces.SVMPeerAcceptResourceModel
+		body.State = "peered"
+		err = interfaces.UpdateSVMPeers(errorHandler, *peerClient, body, data.ID.ValueString())
+		if err != nil {
+			return
+		}
+		restInfo, err = interfaces.GetSVMPeer(errorHandler, *client, data.ID.ValueString())
+		if err != nil {
+			// error reporting done inside GetSVMPeers
+			return
+		}
+	}
+
+	if restInfo != nil {
+		data.State = types.StringValue(restInfo.State)
+	} else {
+		data.State = types.StringValue(resource.State)
+	}
+
+	tflog.Trace(ctx, "created a resource")
+
+	// Save data into Terraform state
+	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
+}
+
+// Update updates the resource and sets the updated Terraform state on success.
+func (r *SVMPeersResource) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
+	var plan, state *SVMPeersResourceModel
+
+	// Read Terraform plan data into the model
+	resp.Diagnostics.Append(req.Plan.Get(ctx, &plan)...)
+	// Read Terraform state data into the model
+	resp.Diagnostics.Append(req.State.Get(ctx, &state)...)
+	errorHandler := utils.NewErrorHandler(ctx, &resp.Diagnostics)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	client, err := connection.GetRestClient(errorHandler, r.config, state.CxProfileName)
+	if err != nil {
+		// error reporting done inside NewClient
+		return
+	}
+
+	if !plan.Peer.PeerCxProfileName.IsNull() && state.State.ValueString() == "initiated" {
+		peerClient, err := connection.GetRestClient(errorHandler, r.config, plan.Peer.PeerCxProfileName)
+		if err != nil {
+			// error reporting done inside NewClient
+			return
+		}
+		var bodyPeer interfaces.SVMPeerAcceptResourceModel
+		bodyPeer.State = "peered"
+		err = interfaces.UpdateSVMPeers(errorHandler, *peerClient, bodyPeer, state.ID.ValueString())
+		if err != nil {
+			return
+		}
+	}
+
+	isEqual := reflect.DeepEqual(plan.Applications, state.Applications)
+
+	if plan.Applications != nil && !isEqual {
+		var applications []string
+		for _, e := range plan.Applications {
+			applications = append(applications, e.ValueString())
+		}
+		var body interfaces.SVMPeerUpdateResourceModel
+		body.Applications = applications
+		err = interfaces.UpdateSVMPeers(errorHandler, *client, body, plan.ID.ValueString())
+		if err != nil {
+			return
+		}
+	}
+
+	restInfo, err := interfaces.GetSVMPeer(errorHandler, *client, plan.ID.ValueString())
+	if err != nil {
+		// error reporting done inside GetSVMPeers
+		return
+	}
+
+	plan.State = types.StringValue(restInfo.State)
+
+	tflog.Debug(ctx, fmt.Sprintf("updated svm peer resource: UUID=%s", plan.ID))
+	// Save updated data into Terraform state
+	resp.Diagnostics.Append(resp.State.Set(ctx, &plan)...)
+}
+
+// Delete deletes the resource and removes the Terraform state on success.
+func (r *SVMPeersResource) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
+	var data *SVMPeersResourceModel
+
+	// Read Terraform prior state data into the model
+	resp.Diagnostics.Append(req.State.Get(ctx, &data)...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	errorHandler := utils.NewErrorHandler(ctx, &resp.Diagnostics)
+	client, err := connection.GetRestClient(errorHandler, r.config, data.CxProfileName)
+	if err != nil {
+		// error reporting done inside NewClient
+		return
+	}
+
+	if data.ID.IsNull() {
+		errorHandler.MakeAndReportError("UUID is null", "svm_peers UUID is null")
+		return
+	}
+
+	err = interfaces.DeleteSVMPeers(errorHandler, *client, data.ID.ValueString())
+	if err != nil {
+		return
+	}
+
+}
+
+// ImportState imports a resource using ID from terraform import command by calling the Read method.
+func (r *SVMPeersResource) ImportState(ctx context.Context, req resource.ImportStateRequest, resp *resource.ImportStateResponse) {
+	idParts := strings.Split(req.ID, ",")
+
+	if len(idParts) != 4 || idParts[0] == "" || idParts[1] == "" || idParts[2] == "" || idParts[3] == "" {
+		resp.Diagnostics.AddError(
+			"Unexpected Import Identifier",
+			fmt.Sprintf("Expected import identifier with format: svm_name,peer_svm_name,peer_cluster_name,cx_profile_name. Got: %q", req.ID),
+		)
+		return
+	}
+
+	resp.Diagnostics.Append(resp.State.SetAttribute(ctx, path.Root("svm").AtName("name"), idParts[0])...)
+	resp.Diagnostics.Append(resp.State.SetAttribute(ctx, path.Root("peer").AtName("svm").AtName("name"), idParts[1])...)
+	resp.Diagnostics.Append(resp.State.SetAttribute(ctx, path.Root("peer").AtName("cluster").AtName("name"), idParts[2])...)
+	resp.Diagnostics.Append(resp.State.SetAttribute(ctx, path.Root("cx_profile_name"), idParts[3])...)
+}
