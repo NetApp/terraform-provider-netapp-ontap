ff6bdc30344708dd63b2130b47e6fb9bf7753f63
Suhas Bangalore Shekar <bsuhas@netapp.com>
2024-08-28 11:41:08 -0700

    new resource qos policies

---
+++ examples/data-sources/netapp-ontap_qos_policies/data-source.tf
@@ -0,0 +1,8 @@
+data "netapp-ontap_qos_policies" "qos_policies" {
+  # required to know which system to interface with
+  cx_profile_name = "cluster1"
+  filter = {
+    svm_name = "terraform"
+    name = "test2"
+  }
+}
