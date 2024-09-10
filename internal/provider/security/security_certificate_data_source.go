53c18330783d8a8dd8aae9febe8506612fcc4aa3
Chandrakanta Sahu <csahu@netapp.com>
2024-09-10 15:23:20 +0530

    add data source config validators

--- internal/provider/security/security_certificate_data_source.go
+++ internal/provider/security/security_certificate_data_source.go
@@ -8,6 +8,8 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/datasource"
 	"github.com/hashicorp/terraform-plugin-framework/datasource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/hashicorp/terraform-plugin-framework/path"
+	"github.com/hashicorp/terraform-plugin-framework-validators/datasourcevalidator"
 	"github.com/hashicorp/terraform-plugin-log/tflog"
 	"github.com/netapp/terraform-provider-netapp-ontap/internal/interfaces"
 	"github.com/netapp/terraform-provider-netapp-ontap/internal/utils"
@@ -70,16 +72,16 @@ func (d *SecurityCertificateDataSource) Schema(ctx context.Context, req datasour
 				MarkdownDescription: "Common name of the certificate.",
 				Optional:            true,
 			},
+			"type": schema.StringAttribute{
+				MarkdownDescription: "Type of Certificate.",
+				Optional:            true,
+			},
 			"svm_name": schema.StringAttribute{
 				MarkdownDescription: "SVM name in which the certificate is installed.",
 				Optional:            true,
 			},
 			"scope": schema.StringAttribute{
 				MarkdownDescription: "Set to 'svm' for certificates installed in a SVM. Otherwise, set to 'cluster'.",
-				Computed:            true,
-			},
-			"type": schema.StringAttribute{
-				MarkdownDescription: "Type of Certificate.",
 				Computed:            true,
 			},
 			"serial_number": schema.StringAttribute{
@@ -126,6 +128,20 @@ func (d *SecurityCertificateDataSource) Configure(ctx context.Context, req datas
 	d.config.ProviderConfig = config
 }
 
+// ConfigValidators validates entire data source configurations
+func (d *SecurityCertificateDataSource) ConfigValidators(ctx context.Context) []datasource.ConfigValidator {
+    return []datasource.ConfigValidator{
+        datasourcevalidator.AtLeastOneOf(
+            path.MatchRoot("name"),
+            path.MatchRoot("common_name"),
+        ),
+		datasourcevalidator.RequiredTogether(
+            path.MatchRoot("common_name"),
+			path.MatchRoot("type"),
+        ),
+    }
+}
+
 // Read refreshes the Terraform state with the latest data.
 func (d *SecurityCertificateDataSource) Read(ctx context.Context, req datasource.ReadRequest, resp *datasource.ReadResponse) {
 	var data SecurityCertificateDataSourceModel
@@ -145,7 +161,17 @@ func (d *SecurityCertificateDataSource) Read(ctx context.Context, req datasource
 		return
 	}
 
-	restInfo, err := interfaces.GetSecurityCertificate(errorHandler, *client, data.Name.ValueString(), data.CommonName.ValueString())
+	cluster, err := interfaces.GetCluster(errorHandler, *client)
+	if err != nil {
+		// error reporting done inside GetCluster
+		return
+	}
+	if cluster == nil {
+		errorHandler.MakeAndReportError("No cluster found", "cluster not found")
+		return
+	}
+
+	restInfo, err := interfaces.GetSecurityCertificate(errorHandler, *client, cluster.Version, data.Name.ValueString(), data.CommonName.ValueString(), data.Type.ValueString())
 	if err != nil {
 		// error reporting done inside GetSecurityCertificate
 		return
