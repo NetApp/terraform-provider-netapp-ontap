c02314e1d0d36f5ba14d006a28eb48fc2e1fd5d5
Chandrakanta Sahu <csahu@netapp.com>
2024-10-03 13:37:14 +0530

    update exmaples

--- examples/data-sources/netapp-ontap_security_certificate/data-source.tf
+++ examples/data-sources/netapp-ontap_security_certificate/data-source.tf
@@ -2,5 +2,5 @@
 data "netapp-ontap_security_certificate" "security_certificate1" {
   # required to know which system to interface with
   cx_profile_name = "cluster5"
-  name            = "tfsvm_17B9B4C1696136FC"
+  name            = "tfsvm_ca_cert1"
 }
