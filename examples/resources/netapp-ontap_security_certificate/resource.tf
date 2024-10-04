98052e82ec73f75def53e57beadaf94deda807a4
Chandrakanta Sahu <csahu@netapp.com>
2024-10-04 16:21:57 +0530

    updated examples

--- examples/resources/netapp-ontap_security_certificate/resource.tf
+++ examples/resources/netapp-ontap_security_certificate/resource.tf
@@ -1,20 +1,20 @@
 # creating a certificate
 resource "netapp-ontap_security_certificate" "create_certificate" {
   cx_profile_name = "cluster5"
-  name            = "svm2_ca_cert1_unique"
-  common_name     = "svm2_ca_cert1"
+  name            = "tfsvm_ca_cert1"
+  common_name     = "tfsvm_ca_cert"
   type            = "root_ca"
-  svm_name        = "svm2"
+  svm_name        = "tfsvm"
   expiry_time     = "P365DT"
 }
 
 # signing a certificate
 resource "netapp-ontap_security_certificate" "sign_certificate" {
   cx_profile_name = "cluster5"
-  name            = "svm2_ca_cert1_unique"
-  common_name     = "svm2_ca_cert1"
+  name            = "tfsvm_ca_cert1"
+  common_name     = "tfsvm_ca_cert"
   type            = "root_ca"
-  svm_name        = "svm3"
+  svm_name        = "svm1"
   expiry_time     = "P90DT"
   signing_request = <<-EOT
 -----BEGIN CERTIFICATE REQUEST-----
@@ -26,9 +26,9 @@ EOT
 # installing a certificate
 resource "netapp-ontap_security_certificate" "install_certificate" {
   cx_profile_name = "cluster5"
-  common_name     = "svm3_cert1"
+  common_name     = "svm1_cert1"
   type            = "server"
-  svm_name        = "svm3"
+  svm_name        = "svm1"
   expiry_time     = "P90DT"
   public_certificate = <<-EOT
 -----BEGIN CERTIFICATE-----
