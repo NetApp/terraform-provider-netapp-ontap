f6af8b15e5d378054e9048626baabc68f1d6422f
Chuyi Ching <chuyic@netapp.com>
2024-10-11 17:11:38 -0700

    update examples

--- examples/data-sources/netapp-ontap_security_login_messages/data-source.tf
+++ examples/data-sources/netapp-ontap_security_login_messages/data-source.tf
@@ -2,6 +2,6 @@ data "netapp-ontap_security_login_messages" "security_login_messages" {
   # required to know which system to interface with
   cx_profile_name = "cluster4"
   filter = {
-    scope = "cluster"
+    scope = "svm"
   }
 }
--- examples/resources/netapp-ontap_nfs_export_policy/resource.tf
+++ examples/resources/netapp-ontap_nfs_export_policy/resource.tf
@@ -1,5 +1,5 @@
 resource "netapp-ontap_nfs_export_policy" "example" {
   cx_profile_name = "cluster4"
   svm_name = "carchi-test"
-  name = "exportpolicytesta"
+  name = "exportpolicytest"
 }
