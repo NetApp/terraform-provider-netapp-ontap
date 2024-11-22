1ff4af37257fd07784c5adf180ee13e218709030
Achim Christ <achim.christ@sva.de>
2024-11-22 14:10:16 +0000

    feat: add examples for using node & port-, and broadcast domain for location
    Signed-off-by: Achim Christ <achim.christ@sva.de>

--- examples/resources/netapp-ontap_network_ip_interface/resource.tf
+++ examples/resources/netapp-ontap_network_ip_interface/resource.tf
@@ -1,15 +1,35 @@
-resource "netapp-ontap_network_ip_interface" "ip_interface" {
+resource "netapp-ontap_network_ip_interface" "with_node_port" {
   # required to know which system to interface with
   cx_profile_name = "cluster4"
   name            = "testme"
   svm_name        = "ansibleSVM"
+
   ip = {
     address = "10.10.10.10"
     netmask = 20
   }
+
   location = {
-    home_port = "e0c"
-    home_node = "ontap_cluster_1-01"
+    home_port = "e0a-300"
+    home_node = "netapp_single-01"
+  }
+}
+
+resource "netapp-ontap_network_ip_interface" "with_broadcast_domain" {
+  # required to know which system to interface with
+  cx_profile_name = "cluster4"
+  name            = "testme"
+  svm_name        = "ansibleSVM"
+
+  ip = {
+    address = "10.10.10.20"
+    netmask = 20
+  }
+
+  location = {
+    broadcast_domain = {
+      name = "tf_test_mgmt_svm02"
+    }
   }
   service_policy = "default-management"
 }
