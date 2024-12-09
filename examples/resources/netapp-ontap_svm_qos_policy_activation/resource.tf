db62b9dcb6f2849b3f1f31210c797c8524da22fd
Achim Christ <achim.christ@sva.de>
2024-12-09 13:09:45 +0000

    chore: cleanup example
    Signed-off-by: Achim Christ <achim.christ@sva.de>

--- examples/resources/netapp-ontap_svm_qos_policy_activation/resource.tf
+++ examples/resources/netapp-ontap_svm_qos_policy_activation/resource.tf
@@ -1,12 +1,9 @@
 resource "netapp-ontap_svm_qos_policy_activation" "example" {
-  cx_profile_name = "svacluster"
+  cx_profile_name = "cluster4"
   svm = {
-    # id = "b366fb39-8d2d-11ef-9ca8-00a0b8bc0407"
     name = "svm02"
   }
   qos_policy = {
-    # id   = "beb86559-8d2d-11ef-9ca8-00a0b8bc0407"
-    name = "test-svm02"
-    # name = "performance-svm02"
+    name = "performance-svm02"
   }
 }
