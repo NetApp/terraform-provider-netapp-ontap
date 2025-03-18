d4133dc5a09e1d261235c2990a2ea16aa46bd1e9
Achim Christ <achim.christ@sva.de>
2025-03-18 14:10:48 +0000

    feat: add `iscsi_service` resource - create/delete method
    Signed-off-by: Achim Christ <achim.christ@sva.de>

---
+++ examples/resources/netapp-ontap_iscsi_service/resource.tf
@@ -0,0 +1,9 @@
+resource "netapp-ontap_iscsi_service" "protocols_iscsi_service" {
+  # required to know which system to interface with
+  cx_profile_name = "svacluster"
+  svm_name        = "svm02"
+  # enabled         = true
+  # target = {
+  #   alias = "test"
+  # }
+}
