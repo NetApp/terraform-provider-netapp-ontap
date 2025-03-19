9185f247103eae89517ec9ccfae2e1ce7a7360c1
Achim Christ <achim.christ@sva.de>
2025-03-19 14:39:25 +0000

    feat: add `iscsi_service` documentation
    Signed-off-by: Achim Christ <achim.christ@sva.de>

--- examples/data-sources/netapp-ontap_iscsi_services/data-source.tf
+++ examples/data-sources/netapp-ontap_iscsi_services/data-source.tf
@@ -1,6 +1,6 @@
 data "netapp-ontap_iscsi_services" "protcols_iscsi_services" {
   # required to know which system to interface with
-  cx_profile_name = "svacluster"
+  cx_profile_name = "cluster2"
   filter = {
     svm_name = "svm*"
   }
--- examples/resources/netapp-ontap_iscsi_service/resource.tf
+++ examples/resources/netapp-ontap_iscsi_service/resource.tf
@@ -1,9 +1,9 @@
 resource "netapp-ontap_iscsi_service" "protocols_iscsi_service" {
   # required to know which system to interface with
-  cx_profile_name = "svacluster"
+  cx_profile_name = "cluster2"
   svm_name        = "svm02"
   enabled         = true
   target = {
-    alias = "alias01"
+    alias = "cl2vs2"
   }
 }
