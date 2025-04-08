0fef992e569697aa026579d5f620472e8a04ece5
Chandrakanta Sahu <csahu@netapp.com>
2025-04-08 12:35:53 +0530

    added example

--- examples/resources/netapp-ontap_volume/resource.tf
+++ examples/resources/netapp-ontap_volume/resource.tf
@@ -1,3 +1,4 @@
+# creating a volume
 resource "netapp-ontap_volume" "example" {
   cx_profile_name = "cluster4"
   name = "terraformTest2"
@@ -9,6 +10,30 @@ resource "netapp-ontap_volume" "example" {
   ]
   space = {
     size = 20
+    size_unit = "mb"
+  }
+}
+
+# creating a volume with autosize options
+resource "netapp-ontap_volume" "example" {
+  cx_profile_name = "cluster4"
+  name = "terraformTest3"
+  svm_name = "ansibleSVM"
+  aggregates = [
+    {
+      name = "aggr2"
+    },
+  ]
+  space = {
+    size = 50
+    size_unit = "mb"
+  }
+  autosize = {
+    minimum = 20
+    maximum = 60
+    shrink_threshold = 10
+    grow_threshold = 90
+    mode = "off"
     size_unit = "mb"
   }
 }
