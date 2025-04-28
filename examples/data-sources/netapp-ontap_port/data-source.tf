53a47631da96400cfcb0f7fc73dbcc5d8b44fc43
Achim Christ <achim.christ@sva.de>
2025-04-28 18:19:59 +0000

    fix: add node name to ethernet_port data source
    Signed-off-by: Achim Christ <achim.christ@sva.de>

--- examples/data-sources/netapp-ontap_port/data-source.tf
+++ examples/data-sources/netapp-ontap_port/data-source.tf
@@ -2,16 +2,25 @@ data "netapp-ontap_port" "physical" {
   # required to know which system to interface with
   cx_profile_name = "cluster4"
   name            = "e0a"
+  node = {
+    name = "netapp_single-01"
+  }
 }
 
 data "netapp-ontap_port" "lag" {
   # required to know which system to interface with
   cx_profile_name = "cluster4"
   name            = "a0a"
+  node = {
+    name = "netapp_single-01"
+  }
 }
 
 data "netapp-ontap_port" "vlan" {
   # required to know which system to interface with
   cx_profile_name = "cluster4"
-  name            = "e0a-100"
+  name            = "e0a-101"
+  node = {
+    name = "netapp_single-01"
+  }
 }
