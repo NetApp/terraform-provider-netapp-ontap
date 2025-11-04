6df117f5e44274c700faacbea061e3e879074620
bdhivya <bdhivya@netapp.com>
2025-11-04 16:11:46 -0500

    Added test file

--- internal/provider/protocols/protocols_s3_policy_resource.go
+++ internal/provider/protocols/protocols_s3_policy_resource.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/netapp/terraform-provider-netapp-ontap/internal/provider/connection"
 
+	"github.com/hashicorp/terraform-plugin-framework/attr"
 	"github.com/hashicorp/terraform-plugin-framework/diag"
 	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
