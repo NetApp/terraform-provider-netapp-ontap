4b5d1a3bb79aa54d661aafeac45bbfb0d2a78eff
bdhivya <bdhivya@netapp.com>
2026-05-12 00:56:54 -0400

    Updated restore_to example in resource
    Signed-off-by: bdhivya <bdhivya@netapp.com>

--- examples/resources/netapp-ontap_volume/resource.tf
+++ examples/resources/netapp-ontap_volume/resource.tf
@@ -22,3 +22,33 @@ resource "netapp-ontap_volume" "example1" {
   }
   snapshot_locking_enabled = true
 }
+
+# restoring a volume from snapshot
+resource "netapp-ontap_volume" "example2" {
+  cx_profile_name = "cluster4"
+  name = "terraformTest2"
+  svm_name = "ansibleSVM"
+  aggregates = [
+    {
+      name = "aggr1"
+    },
+  ]
+  space = {
+    size = 50
+    size_unit = "mb"
+  }
+  autosize = {
+    minimum = 40
+    maximum = 60
+    shrink_threshold = 10
+    grow_threshold = 90
+    mode = "grow"
+    size_unit = "mb"
+  }
+  snapshot_locking_enabled = true
+  restore_to = {
+    snapshot = {
+      name = "snap1"
+    }
+  }
+}
