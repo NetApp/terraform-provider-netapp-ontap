8c3231f497bfd16352077c2e620eac05743015a5
Chandrakanta Sahu <csahu@netapp.com>
2026-05-19 12:17:18 +0530

    added docs for new data source and resource unix_user

--- scripts/generate_docs.py
+++ scripts/generate_docs.py
@@ -50,10 +50,13 @@ CATEGORIES = {
     'name-services': [
         "name_services_dns_data_source.md",
         "name_services_dnss_data_source.md",
-        "name_services_dns_resource.md"
+        "name_services_dns_resource.md",
         "name_services_ldap_data_source.md",
         "name_services_ldaps_data_source.md",
         "name_services_ldap_resource.md",
+        "name_services_unix_user_data_source.md",
+        "name_services_unix_users_data_source.md",
+        "name_services_unix_user_resource.md",
     ],
     'ndmp': [],
     'networking': [
